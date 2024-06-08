19b9c25b9fe1947b0de732231b89f567ff47e454
Niranjan Suresh <131999284+Major616@users.noreply.github.com>
2024-06-08 11:02:31 +0530

    committed

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun clickHandler(view: View) {
+       Log.i("clickHandler","Button clicked")
+        var dialIntent: Intent = Intent(Intent.ACTION_DIAL)
+        startActivity(dialIntent)
+    }
 }
+
